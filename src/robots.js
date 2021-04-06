4cb22e7f796aa8717ca529e9040029f65c5b8d67
shivkumarkashyapbhai.guard@georgebrown.ca <42621966+kashyap1808@users.noreply.github.com>
2021-04-05 21:51:41 -0400

    Robot Friends React Application

---
+++ src/robots.js
@@ -0,0 +1,86 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'John Doe',
+        username: 'JD',
+        email: 'jd@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Jae Doe',
+        username: 'JaeyD',
+        email: 'jae@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Johny Walker',
+        username: 'JW',
+        email: 'johny@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Alice Walker',
+        username: 'AW',
+        email: 'aw@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Hank Smith',
+        username: 'hanky',
+        email: 'hanky@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Gujjar Maan',
+        username: 'GM',
+        email: 'gujjar@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Smith Ross',
+        username: 'Ross',
+        email: 'break@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Chandler Bing',
+        username: 'Bong',
+        email: 'cb@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Salman Khan',
+        username: 'Sallu',
+        email: 'terabaap@gmail.com  '
+    },
+    {
+        id: 10,
+        name: 'Vishwa Bhoot',
+        username: 'Chudail',
+        email: 'imdaayan@gmail.com'
+    },
+    {
+        id: 11,
+        name: 'Shiv Guard',
+        username: 'Shivlo',
+        email: 's@g.com'
+    },
+    {
+        id: 12,
+        name: 'Karan Patel',
+        username: 'Kaju',
+        email: 'k@patel.com'
+    },
+    {
+        id: 13,
+        name: 'Nishtha Ramani',
+        username: 'Aloo Petis',
+        email: 'bham@gmail.com'
+    },
+    {
+        id: 14,
+        name: 'Kashyap Gauswami',
+        username: 'Kaspi',
+        email: 'k@g.com'
+    }
+];
